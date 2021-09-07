7f5d4bce7c0f0e2f0feda6dd715cc9e3c15b9c4c
craig410 <craig@netchaos.net>
2021-09-07 11:58:42 +0100

    Update phpspec so we can test on PHP8

--- koharness.php
+++ koharness.php
@@ -1,3 +1,6 @@
 <?php
 // Configuration for koharness - builds a standalone skeleton Kohana app for running specs
-return array();
+return [
+    'modules' => [],
+    'syspath' => __DIR__.'/vendor/ingenerator/kohana-core',
+];
